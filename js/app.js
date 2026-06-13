57a76e207070bc7211695e7095a61f90d225135e
GR TESTE <anthonygrupopralog@gmail.com>
2026-06-13 01:35:39 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-pdfjsLib.GlobalWorkerOptions.workerSrc = 'libs/pdf.worker.min.js';
+pdfjsLib.GlobalWorkerOptions.workerSrc =   'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js';
 
 let arquivos = [];
 let resultados = [];
